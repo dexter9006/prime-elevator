9826425c33a980303751247a5846a101326e5078
dexter9006 <52006237+dexter9006@users.noreply.github.com>
2022-08-27 17:41:21 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.UseLanguage = "en"
+Config.UseLanguage = "fr"
 Config.UseSoundEffect = true
 
 Config.Elevators = {
@@ -9,19 +9,19 @@ Config.Elevators = {
         Sound = "LiftSoundBellRing",
         Floors = {
             [1] = {
-                Label = "Second Floor",
+                Label = "Deuxième étage",
                 FloorDesc = "Hospital Rooftop",
                 Coords = vector3(338.51, -583.81, 74.16),
                 ExitHeading = "250.07"
             },
             [2] = {
-                Label = "First Floor",
+                Label = "Premier étage",
                 FloorDesc = "Hospital Upper Lobby",
                 Coords = vector3(327.02, -603.85, 43.28),
                 ExitHeading = "337.25"
             },
             [3] = {
-                Label = "Ground Floor",
+                Label = "Rez-de-chaussée",
                 FloorDesc = "Hospital Garage",
                 Coords = vector3(340.18, -584.68, 28.8),
                 ExitHeading = "104.87"
@@ -50,19 +50,18 @@ Config.Elevators = {
 
 Config.PolyZone = false -- This will show the box zones | If set to false it will hide the box zones
 
-Config.WaitTime = 8000 -- This will set the time for the ProgressBar | 1000 = 1 second
+Config.WaitTime = 5 * 1000 -- This will set the time for the ProgressBar | 1000 = 1 second
 
 Config.ElevatorButton = {
 
     ------ / Pill Box Hospital
     -- UnRestricted
-    [1] = { name = "Ground Floor", location = vector3(346.10, -581.00, 28.8), width = 0.1, length = 0.3, heading = 69.47, minz = 28.9, maxz = 29.3},
-    [2] = { name = "First Floor", location = vector3(330.04, -602.7, 43.28), width = 0.2, length = 0.3, heading = 247.68, minz = 43.48, maxz = 43.78 },
+    [1] = { name = "Rez-de-chaussée", location = vector3(346.10, -581.00, 28.8), width = 0.1, length = 0.3, heading = 69.47, minz = 28.9, maxz = 29.3},
+    [2] = { name = "Premier étage", location = vector3(330.04, -602.7, 43.28), width = 0.2, length = 0.3, heading = 247.68, minz = 43.48, maxz = 43.78 },
 
-    -- Restricted
     [3] = { name = "EMS Garage", location = vector3(339.70, -586.20, 28.8), width = 0.1, length = 0.3, heading = 246.66, minz = 28.9, maxz = 29.3},
-    [4] = { name = "Main Floor", location = vector3(325.65, -603.39, 43.28), width = 0.1, length = 0.3, heading = 160.6, minz = 43.48, maxz = 43.78},
-    [5] = { name = "Heli Pad", location = vector3(338.42, -583.71, 74.16), width = 0.5, length = 2.8, heading = 70.21, minz = 74.16 - 1, maxz = 74.16 + 1.5 },
+    [4] = { name = "Étage principal", location = vector3(325.65, -603.39, 43.28), width = 0.1, length = 0.3, heading = 160.6, minz = 43.48, maxz = 43.78},
+    [5] = { name = "Heli-Pad", location = vector3(338.42, -583.71, 74.16), width = 0.5, length = 2.8, heading = 70.21, minz = 74.16 - 1, maxz = 74.16 + 1.5 },
 
 }
 
@@ -73,4 +72,11 @@ Config.Language = {
         CurrentFloor = "Current Floor: ",
         Unable = "You Can't Use The Elevator...",
     },
+    ["fr"] = {
+        Waiting = "L'ascenseur arrive...",
+        Restricted = "Accès Restreint!",
+        CurrentFloor = "Étage actuel: ",
+        Unable = "Vous ne pouvez pas utiliser l'ascenseur...",
+    },
+}
 }
